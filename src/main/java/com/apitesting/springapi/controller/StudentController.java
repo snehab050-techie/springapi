f2e14ea436ba8af813c572365000ee51f660ca07
snehab050-techie <snehab050@gmail.com>
2026-05-05 17:13:30 +0530

    Committing changes to the StudentController and Student entity, and adding a new StudentRepository interface. The StudentController now directly interacts with the StudentRepository to fetch student data, which is not a good practice as it bypasses the service layer. The service layer should be used to handle business logic and interact with the repository.

--- src/main/java/com/apitesting/springapi/controller/StudentController.java
+++ src/main/java/com/apitesting/springapi/controller/StudentController.java
@@ -3,15 +3,27 @@ package com.apitesting.springapi.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apitesting.springapi.dto.StudentDto;
+import com.apitesting.springapi.entity.Student;
+import com.apitesting.springapi.repository.StudentRepository;
+
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class StudentController {
 
+    //Here we are directly using the repository in the controller, which is not a good practice. 
+    // Communicating directly from presentation layer to data access or persistence layer is not a good practice.
+    // We should use a service layer to handle the business logic and then call the repository from the service layer. 
+    private final StudentRepository studentRepository;
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @GetMapping("/student")
-    public StudentDto getStudent(){
-        return new StudentDto(101,"Sneha Kaggal","sneha@gmail.com");
+    public List<Student> getStudent(){
+        return studentRepository.findAll();
     }
 
     @GetMapping("/student/{id}")
---
+++ src/main/java/com/apitesting/springapi/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.apitesting.springapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apitesting.springapi.entity.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> { 
+}
